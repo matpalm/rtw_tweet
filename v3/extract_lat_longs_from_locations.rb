4b979cabc261e494215fdc839fe5a62a41632f59
mat <matthew.kelcey@gmail.com>
2009-10-24 21:34:50 +1100

    border case for north/south pole

--- v3/extract_lat_longs_from_locations.rb
+++ v3/extract_lat_longs_from_locations.rb
@@ -5,8 +5,8 @@ STDIN.each do |line|
 	next unless $1 and $2	
 	lat,lon = $1.to_f,$2.to_f
 	# those crazy kids will put ANYTHING in these days...
-	next if lat < -90 || lat > 90   
-	next if lon < -180 || lon > 180 
+	next if lat <= -90 || lat >= 90   
+	next if lon <= -180 || lon >= 180 
 	next if lat == 0 && lon == 0
 	puts [time, lat, lon].join "\t"
 end
